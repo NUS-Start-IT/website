d2d97080c3e7ae75a94eaa7d8fa6057f722465f8
Nimastic <jerielchanzy@gmail.com>
2024-09-02 15:14:54 +0800

    Add background

--- StartIT Website/src/main.js
+++ StartIT Website/src/main.js
@@ -6,6 +6,8 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTelegram, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
+// @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;600;700&display=swap');
+
 library.add(faTelegram, faInstagram, faLinkedin);
 
 createApp(App)
