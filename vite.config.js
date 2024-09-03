0375071d8b9fc55520b9bc45052eb58766e85d62
Nimastic <jerielchanzy@gmail.com>
2024-09-03 11:14:42 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/website/',
+  base: '',
   plugins: [
     vue(),
     vueJsx(),
